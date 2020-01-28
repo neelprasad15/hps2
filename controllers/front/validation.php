583fb90e971a4a4a7cbd91d7644bbc1cf0394b77
neelprasad15 <neelprasad15@gmail.com>
2020-01-28 18:52:09 +0530

    new_indent

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -12,22 +12,22 @@ class LatpayValidationModuleFrontController extends ModuleFrontController
 {
     public function init()
     {
-      parent::init();
+        parent::init();
     }
-    public function initcontent()
-    {
-      parent::initContent();
-      $this->context->smarty->assign(
-        array(        
-          'Merchant_User_Id' => Tools::getValue("Merchant_User_Id"),
-          'merchantpwd' => Tools::getValue("merchantpwd"),
-          'currencydesc' => Tools::getValue("currencydesc"),
-          'amount' => Tools::getValue("amount"),
-          'processurl' => Tools::getValue("processurl"),
-          'merchant_ref_number' => Tools::getValue("merchant_ref_number")
-        ));  
-      $this->setTemplate('module:latpay/views/templates/front/validation.tpl');
-    }
+        public function initcontent()
+        {
+          parent::initContent();
+          $this->context->smarty->assign(
+            array(        
+              'Merchant_User_Id' => Tools::getValue("Merchant_User_Id"),
+              'merchantpwd' => Tools::getValue("merchantpwd"),
+              'currencydesc' => Tools::getValue("currencydesc"),
+              'amount' => Tools::getValue("amount"),
+              'processurl' => Tools::getValue("processurl"),
+              'merchant_ref_number' => Tools::getValue("merchant_ref_number")
+            ));  
+          $this->setTemplate('module:latpay/views/templates/front/validation.tpl');
+        }
   public function postProcess()
     {
       /**
