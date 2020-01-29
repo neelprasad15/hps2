e9f6e10473558a843d2f5f57101b553ce7639fe6
neelprasad15 <neelprasad15@gmail.com>
2020-01-29 12:19:49 +0530

    new_indent

--- controllers/front/form.php
+++ controllers/front/form.php
@@ -8,28 +8,25 @@
  * @link      https://www.latpay.com.au/
  *
  */
-
-class LatpayFormModuleFrontController extends ModuleFrontController
+class LatpayValidationModuleFrontController extends ModuleFrontController
 {
-
-	public function init()
-	{
-		parent::init();
-	}
-
-
-	public function initcontent()
-	{
-		parent::initContent();
-
-		$this->context->smarty->assign(
-		);
-
-		$this->setTemplate('module:latpay/views/templates/front/form.tpl');
-	}
-
-
-
-
-	
+    public function init()
+    {
+        parent::init();
+    }
+    public function initcontent()
+    {
+        parent::initContent();
+        $this->context->smarty->assign(
+            array(
+              'Merchant_User_Id' => Tools::getValue("Merchant_User_Id"),
+              'merchantpwd' => Tools::getValue("merchantpwd"),
+              'currencydesc' => Tools::getValue("currencydesc"),
+              'amount' => Tools::getValue("amount"),
+              'processurl' => Tools::getValue("processurl"),
+              'merchant_ref_number' => Tools::getValue("merchant_ref_number")
+            )
+        );
+        $this->setTemplate('module:latpay/views/templates/front/validation.tpl');
+    }
 }
