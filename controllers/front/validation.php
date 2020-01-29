0904029c5b3317ce5a29f9eafcfadb4486410854
neelprasad15 <neelprasad15@gmail.com>
2020-01-29 11:55:17 +0530

    new_indent

--- controllers/front/validation.php
+++ controllers/front/validation.php
@@ -88,7 +88,7 @@ class LatpayValidationModuleFrontController extends ModuleFrontController
               'reference'      => $reference
             );
             //Cancel order
-        if ($token == "1") {
+            if ($token == "1") {
               // $this->module->validateOrder(
               //         (int) $this->context->cart->id,
               //         Configuration::get('PS_OS_CANCELED'),
@@ -106,9 +106,9 @@ class LatpayValidationModuleFrontController extends ModuleFrontController
                 $this->context->cart = $duplicated_cart['cart'];
                 $this->context->cookie->id_cart = (int)$this->context->cart->id;
                 Tools::redirect('index.php?controller=order&step=1');
-        }
+            }
            //Post transtoken values to capture
-        if ($transtokenval) {
+            if ($transtokenval) {
                 $url = 'https://lateralpayments.com/checkout/Checkout/Capture';
                 $data_json = json_encode($jsonData);
                 $ch = curl_init();
@@ -125,7 +125,7 @@ class LatpayValidationModuleFrontController extends ModuleFrontController
                 $statusdesc  = $jdecode['Capture']['status']['errordesc'];
                 $cart_id = (int)$this->context->cart->id;
             if ($status_code =='0') {
-                    $this->module->validateOrder(
+                $this->module->validateOrder(
                     (int) $this->context->cart->id,
                     Configuration::get('PS_OS_PAYMENT'),
                     (float) $this->context->cart->getOrderTotal(true, Cart::BOTH),
@@ -138,7 +138,7 @@ class LatpayValidationModuleFrontController extends ModuleFrontController
                 );
                 Tools::redirect('index.php?controller=order-confirmation&id_cart='.(int)$cart->id.'&id_module='.(int)$this->module->id.'&id_order='.$this->module->currentOrder.'&key='.$customer->secure_key);
             } elseif ($status_code =='1') {
-                    $this->module->validateOrder(
+                $this->module->validateOrder(
                     (int) $this->context->cart->id,
                     Configuration::get('PS_OS_ERROR'),
                     (float) $this->context->cart->getOrderTotal(true, Cart::BOTH),
@@ -148,14 +148,14 @@ class LatpayValidationModuleFrontController extends ModuleFrontController
                     (int) $this->context->currency->id,
                     false,
                     $customer->secure_key
-                    );
-                    $this->context->cart = new Cart($cart_id);
-                    $duplicated_cart = $this->context->cart->duplicate();
-                    $this->context->cart = $duplicated_cart['cart'];
-                    $this->context->cookie->id_cart = (int)$this->context->cart->id;
-                    Tools::redirect('index.php?controller=order&step=1');
+                );
+                $this->context->cart = new Cart($cart_id);
+                $duplicated_cart = $this->context->cart->duplicate();
+                $this->context->cart = $duplicated_cart['cart'];
+                $this->context->cookie->id_cart = (int)$this->context->cart->id;
+                Tools::redirect('index.php?controller=order&step=1');
             }
-        }
-        }
+            }
+            }
     }
 }
